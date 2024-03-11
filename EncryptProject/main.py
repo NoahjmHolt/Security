e9d7d57d47472022f6d280768bf43d6881e1229e
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-03-11 16:04:49 -0600

    Big project
    Starting big project

---
+++ EncryptProject/main.py
@@ -0,0 +1,26 @@
+'''
+
+Noah Holt
+
+Here is the Big project for Computer Security
+
+We have set out to create the DES, 3DES, and AES encryptions
+Side Goal: decorate with time functions to see and compare
+            the 3 in terms of time as stated in class
+
+
+'''
+
+
+from numpy import random
+import timeit
+
+# thanks to https://stackoverflow.com/questions/2512225/matplotlib-plots-not-showing-up-in-mac-osx
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
